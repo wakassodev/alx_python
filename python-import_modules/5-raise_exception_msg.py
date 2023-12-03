2e062c67cabf1d7eec5dfcc21d91e2391e4a2120
wakassodev <wakasso94@gmail.com>
2023-12-03 18:01:22 +0400

    Add to numbers

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
+    raise NameError(message)
+
+if __name__ == "__main__":
     try:
-        raise NameError(message)
-    except NameError as e:
-        print(f"Caught a name exception: {e}")
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
