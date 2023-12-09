09055690be711035058bb9b97e2800ae94579a3f
wakassodev <wakasso94@gmail.com>
2023-12-09 21:55:28 +0400

    Python - Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 """Contains the class BaseGeometry"""
 class BaseGeometry:
-    pass
+    """Empty class."""
+
+    def __dir__(self):
+        """Exclude __init_subclass__ from the output of dir()."""
+        return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
