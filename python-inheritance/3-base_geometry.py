4dd0591996f0eae69e545415d1350765d1a2c964
wakassodev <wakasso94@gmail.com>
2023-12-09 21:36:29 +0400

    Python - Inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,3 +7,8 @@ Contains the class BaseGeometry
 class BaseGeometry:
     """An empty class"""
     pass
+
+if __name__ == "__main__":
+    BaseGeometry = __import__('3-base_geometry').BaseGeometry
+
+    bg = BaseGeometry()
