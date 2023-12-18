eb524978dc502cfd3dc35fe22d8f52fd511e962f
wakassodev <wakasso94@gmail.com>
2023-12-19 00:17:01 +0400

    Python - Network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
-import requests
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import requests
 
 if __name__ == '__main__':
 
