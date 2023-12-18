431fd1bf4812f9e456ccd4ce6d2e79c20fe27c2f
wakassodev <wakasso94@gmail.com>
2023-12-19 00:28:38 +0400

    Python - Network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """A script that
-- uses urlib package
+- uses requests and sys packages
 """
-from urllib import requests
+import requests
+import sys
 
 """
 - fetches https://alx-intranet.hbtn.io/status.
 """
 if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
     
-    with requests.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    # Sending a GET request to the URL
+    response = requests.get(url)
+    
+    # Retrieving the value of the X-Request-Id header
+    x_request_id = response.headers.get('X-Request-Id')
+    
+    # Displaying the X-Request-Id value
+    print(x_request_id)
