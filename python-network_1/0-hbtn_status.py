b20c03b2639b49a1b4659ac0de275c08acabfb89
wakassodev <wakasso94@gmail.com>
2023-12-19 00:30:57 +0400

    Python - Network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 """A script that
-- uses requests and sys packages
+- uses the requests package
 """
 import requests
-import sys
 
 """
 - fetches https://alx-intranet.hbtn.io/status.
 """
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    
-    # Sending a GET request to the URL
     response = requests.get(url)
-    
-    # Retrieving the value of the X-Request-Id header
-    x_request_id = response.headers.get('X-Request-Id')
-    
-    # Displaying the X-Request-Id value
-    print(x_request_id)
+
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
