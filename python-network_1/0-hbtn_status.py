5dfe5be730a1fa36192ba536f015616e1205fe7e
wakassodev <wakasso94@gmail.com>
2023-12-19 00:33:34 +0400

    Python - Network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,5 +11,6 @@ if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
 
-    print("\t- type:", type(response))
-    print("\t- content:", response)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
