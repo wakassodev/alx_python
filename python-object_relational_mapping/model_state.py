697a266e74cb417d174284cf8e85208d8c5c729d
wakassodev <wakasso94@gmail.com>
2024-01-16 23:35:15 +0400

    Python - Object-relational mapping -1

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,9 +3,20 @@
 """
 import sys
 from model_state import Base, State
-
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Create a connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and display State objects
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
