88809ae65bcf85e367ef99c441180bba5c9deee7
wakassodev <wakasso94@gmail.com>
2024-01-16 23:39:52 +0400

    Python - Object-relational mapping -1

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Script that lists all State objects from the database hbtn_0e_6_usa"""
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     # Create a connection to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
