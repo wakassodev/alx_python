31dc062727f3bef3bc1386ee6f1c7012d05330a2
wakassodev <wakasso94@gmail.com>
2024-01-24 10:53:49 +0400

    strict_slashes=False

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this module starts a Flask Web application
+"""
+strict_slashes=False
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """flask hello world"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
